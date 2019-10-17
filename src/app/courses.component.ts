53cb5bda43721e9db88dca5cb3481a7927d52428
Neelaka-Rupun <rnelaka@gmail.com>
2019-10-17 08:41:26 +0530

    commit2

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,9 +4,20 @@ import { from } from 'rxjs';
 
 @Component({
   selector : 'app-courses',
-  template : '<h2>Courses</h2>',
+  template : `
+  <h2>{{ title }}</h2>
+<ul>
+<li *ngFor="let course of courses">
+{{course}}</li>
+</ul>
+` //data binding.
 })
 
 export class CoursesComponent {
+  title = 'List of courses';
 
+  // getTitle() {
+  //   return this.title;
+  // }
+  courses = ['course1', 'course2', 'course3'];
 }
